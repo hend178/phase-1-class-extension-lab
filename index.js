032905fac921f81017195f0cff98e1e7f2f796cc
unknown <hindmuhamed.178@gmail.com>
2022-10-16 23:23:25 +0300

    Complete Assigment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Your code here
+class Polygon{
+    constructor(){
+        this.nums=nums;
+    }
+    get countSides(){
+        return this.nums.length;
+
+    }
+    get perimeter(){
+        return this.nums.reduce((ass,curr)=>ass+curr);
+
+    }
+
+}
+class Triangle extends Polygon{
+    get isValid(){
+        let is=false;
+        for(let i=1; i<this.nums.length; i++){
+            if(this.nums[i-1]==this.nums[i]){
+                is=true;
+            }
+            else{
+                is=false
+            }
+        }
+        return is;
+    }
+}
+class Square extends Polygon{
+    get isValid(){
+        let is=false;
+        for(let i=1; i<this.nums.length; i++){
+            if(this.nums[i-1]==this.nums[i]){
+                is=true;
+            }
+            else{
+                is=false
+            }
+        }
+        return is;
+    }
+    get area(){
+        return this.nums[0]**2;
+    }
+}
